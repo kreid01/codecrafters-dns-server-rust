9adc8b5d1b9aabec354f7ec57a125958e8bc0450
Kieran Reid <kieranreid2612@gmail.com>
2026-01-14 17:30:33 +0000

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,14 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = [];
+                let response = vec![
+                    0x04, 0xD2, 0x80, // QR=1, OPCODE=0, AA=0, TC=0, RD=0       │
+                    0x00, // RA=0, Z=0, RCODE=0                     │
+                    0x00, 0x00, // Question Count (QDCOUNT): 0      │
+                    0x00, 0x00, // Answer Record Count (ANCOUNT): 0 │
+                    0x00, 0x00, // Authority Record Count           │
+                    0x00, 0x00, // Additional Record Count          │
+                ];
                 udp_socket
                     .send_to(&response, source)
                     .expect("Failed to send response");
