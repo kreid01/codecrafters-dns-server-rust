326f0d030daee4b1cc8a86e9fc6e41e2e2522f2b
Kieran Reid <kieranreid2612@gmail.com>
2026-01-15 19:56:21 +0000

    Extraced question from bytes and added id

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,23 @@ fn main() {
         match udp_socket.recv_from(&mut buf) {
             Ok((size, source)) => {
                 println!("Received {} bytes from {}", size, source);
-                let response = vec![
-                    0x04, 0xD2, 0x80, // QR=1, OPCODE=0, AA=0, TC=0, RD=0       │
-                    0x00, // RA=0, Z=0, RCODE=0                     │
-                    0x00, 0x00, // Question Count (QDCOUNT): 0      │
-                    0x00, 0x00, // Answer Record Count (ANCOUNT): 0 │
-                    0x00, 0x00, // Authority Record Count           │
-                    0x00, 0x00, // Additional Record Count          │
+
+                let mut pos = 12;
+
+                parse_qname(&buf, &mut pos);
+                pos += 4;
+
+                let question = &buf[12..pos];
+
+                let header_response = vec![
+                    buf[0], buf[1], 0x80, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
                 ];
-                udp_socket
-                    .send_to(&response, source)
-                    .expect("Failed to send response");
+
+                let mut response = Vec::new();
+                response.extend_from_slice(&header_response);
+                response.extend_from_slice(question);
+
+                udp_socket.send_to(&response, source).unwrap();
             }
             Err(e) => {
                 eprintln!("Error receiving data: {}", e);
@@ -27,4 +33,23 @@ fn main() {
             }
         }
     }
+}
+
+fn parse_qname(buf: &[u8], pos: &mut usize) -> String {
+    let mut labels = Vec::new();
+
+    loop {
+        let len = buf[*pos] as usize;
+        *pos += 1;
+
+        if len == 0 {
+            break;
+        }
+
+        let label = &buf[*pos..*pos + len];
+        labels.push(String::from_utf8_lossy(label).to_string());
+        *pos += len;
+    }
+
+    labels.join(".")
 }
